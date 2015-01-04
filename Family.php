6930d783143d7021a3ecd43816ddc009acb92254
Alexander Seeto <seeto.alexander@gmail.com>
2015-01-04 18:49:38 -0500

    Refresh

--- Family.php
+++
@@ -1,8 +0,0 @@
-<script src="js/info/familyinfo.js"></script>
-<script src="js/init/familyinit.js"></script>
-<script src="js/horizontalslidingpanel.js"></script>
-<script src="js/popupprofile.js"></script>
-
-<div id="hiddenpopboxes"></div>
-<div id="fa_l" class="fa left_horiz_slider"></div>
-<div id="fa_r" class="fa right_horiz_slider"></div>
