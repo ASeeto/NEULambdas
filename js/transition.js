df53e99d6cd88ed637b9957e5ec9ffecdc2c7859
ASeeto <seeto.alexander@gmail.com>
2015-03-15 23:31:34 -0400

    Adjusted page title change for EBoards div

--- js/transition.js
+++ js/transition.js
@@ -1,17 +1,21 @@
 $(function() {
-	/** Performs transition to different div to simulate different page. */
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var li = $(this).parent();
-			li.addClass('activemenubutton');
-			li.siblings().removeClass('activemenubutton');
-			var target = $(this.hash);
-			var title = target.selector.slice(1,target.selector.length);
-			var html = '<h1>'+title.charAt(0).toUpperCase()+title.slice(1)+'</h1>';
-			$('#title').html(html);
-			$(target.selector).siblings().fadeOut(300);
-			$(target.selector).delay(400).fadeIn(300);
-			return false;
-		}
-	});
+    /** Performs transition to different div to simulate different page. */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var li = $(this).parent();
+            li.addClass('activemenubutton');
+            li.siblings().removeClass('activemenubutton');
+            var target = $(this.hash);
+            var title = target.selector.slice(1,target.selector.length);
+            var html = '<h1>'+title.charAt(0).toUpperCase()+title.slice(1)+'</h1>';
+            if(html === "<h1>Eboards</h1>"){
+                $('#title').html("<h1>EBoards</h1>")
+            }else{
+                $('#title').html(html);
+            }
+            $(target.selector).siblings().fadeOut(300);
+            $(target.selector).delay(400).fadeIn(300);
+            return false;
+        }
+    });
 });
